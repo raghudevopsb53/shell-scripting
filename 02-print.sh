a23beecc9cabfd44e466dda9c56ebec02e15691d
KalluruS <>
2020-12-07 08:16:32 +0530

    Update shell-scripting/

---
+++ 02-print.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# We can print a message on screen using two commands in linux
+# 1. echo   -> Preferred one , because of less syntax
+# 2. printf  -> Have more syntax
+
+echo Hello World
