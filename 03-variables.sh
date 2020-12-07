4641390bc04ae92d4cfe4c5f3f03b7f12913b3ce
KalluruS <>
2020-12-07 08:48:46 +0530

    Update shell-scripting/

--- 03-variables.sh
+++ 03-variables.sh
@@ -9,3 +9,19 @@ COURSE_NAME=DevOps
 echo Welcome to $COURSE_NAME training
 echo $COURSE_NAME Training Timings - 6AM
 echo $COURSE_NAME Trainer - Raghu
+
+# Declare a variable , VARNAME=DATA
+# TO access variable , $VARNAME
+
+# What characters you can use in variable names
+# a-z, A-Z, 0-9, _(underscore)
+# variable name should not start with a number
+    # 1var is wrong
+    # var1 is right
+
+# STYLE in Variable NAMES
+# Unix/Linux -> VARNAME (All in capital characters)
+# Java - varName (CamelCase)
+
+
+echo "Good Morning, Today date is 2020-12-07"
