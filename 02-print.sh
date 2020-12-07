61fb0b4e7fe7f4718929427f5cd220c7f72a97b7
KalluruS <>
2020-12-07 08:28:53 +0530

    Update shell-scripting/

--- 02-print.sh
+++ 02-print.sh
@@ -34,3 +34,8 @@ echo -e "Hello,\n\n\tWelcome to todays session.\n\nRegards,\nRaghu K."
 # echo -e "\e[COLmMESSAGE"
 
 echo -e "\e[31mRED TEXT"
+echo -e "\e[32mGREEEN TEXT"
+echo -e "\e[33mYELLOW TEXT"
+echo -e "\e[34mBLUE TEXT"
+echo -e "\e[35mMAGENTA TEXT"
+echo -e "\e[36mCYAN TEXT"
