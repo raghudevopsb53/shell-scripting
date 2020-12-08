41a4d06c62f5ff2322fa0bfe79ade1ecd476c12b
KalluruS <>
2020-12-08 08:28:59 +0530

    Update shell-scripting/

--- 05-special-vars.sh
+++ 05-special-vars.sh
@@ -4,6 +4,7 @@ echo Script Name = $0
 echo First Argument = $1
 echo Second Argument = $2
 
-echo All Arguments (*) =  $*
-echo All Arguments (@) = $@
+echo "All Arguments (*) =  $*"
+echo "All Arguments (@) = $@"
+
 
