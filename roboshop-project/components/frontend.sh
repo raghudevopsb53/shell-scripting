fe25f12a3b878a605b8a988fff27416a91f2452e
KalluruS <>
2020-12-09 08:27:19 +0530

    Update shell-scripting/

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-INFO() {
-  echo -e "[\e[1;34mINFO\e[0m] [\e[1;35mfrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] $1"
-}
-
-SUCC() {
-  echo -e "[\e[1;32mSUCC\e[0m] [\e[1;35mfrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] $1"
-}
-
-FAIL() {
-  echo -e "[\e[1;31mFAIL\e[0m] [\e[1;35mfrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] $1"
-}
 
 
 INFO "Setup Frontend Component"
