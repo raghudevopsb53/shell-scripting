aab5a929657c979dca676a08ab9a9420e2887f69
KalluruS <>
2020-12-09 08:11:29 +0530

    Update shell-scripting/

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo [INFO] [frontend] [$(date "+%F %T")] Installing Frontend Component
+echo -e "[\e[1;34mINFO\e[0m] [\e[1;35mfrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] Installing Frontend Component"
