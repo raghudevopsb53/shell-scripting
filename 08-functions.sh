d8e121904f0ab552dcb326e3aa41abcea0f42890
KalluruS <>
2020-12-09 08:18:56 +0530

    Update shell-scripting/

---
+++ 08-functions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Assign a name to data is variable
+# Assign a name to group of commands is called as function
+
+# Declare a  function
+
+# Way 1
+function SAMPLE() {
+  echo Hello from SAMPLE Function
+}
+
+# Way 2
+SAMPLE1() {
+  echo Hello from SAMPLE1 Function
+}
+
+# Function name standards are as same as variables in bash shell
+
+# call a function
+SAMPLE
+SAMPLE1
