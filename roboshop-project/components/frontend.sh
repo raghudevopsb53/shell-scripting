4702804e628280cd93dbb420681016f12677b119
KalluruS <>
2020-12-09 08:13:19 +0530

    Update shell-scripting/

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -3,3 +3,4 @@
 echo -e "[\e[1;34mINFO\e[0m] [\e[1;35mfrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] Setup Frontend Component"
 echo -e "[\e[1;34mINFO\e[0m] [\e[1;35mfrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] Installing Nginx"
 echo -e "[\e[1;32mSUCC\e[0m] [\e[1;35mfrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] Installed Nginx"
+echo -e "[\e[1;31mFAIL\e[0m] [\e[1;35mfrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] Installed Nginx"
