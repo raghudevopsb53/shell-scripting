435f8433839027dfab6e5556858c78370036adc7
KalluruS <>
2020-12-10 08:02:08 +0530

    Update shell-scripting/

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -12,11 +12,6 @@ INFO "Downloading Artifacts"
 DOWNLOAD_ARTIFACT "https://dev.azure.com/DevOps-Batches/f4b641c1-99db-46d1-8110-5c6c24ce2fb9/_apis/git/repositories/a781da9c-8fca-4605-8928-53c962282b74/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
 
 
-
-
-# rm -rf static README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
-
 INFO "Remove Old Artifacts"
 rm -rvf /usr/share/nginx/html/* &>>$LOG_FILE
 STAT $? "Artifact Removal"
