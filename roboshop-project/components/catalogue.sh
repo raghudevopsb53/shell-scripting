5474c39e002e04691db7741d70d709e4970ce0d8
KalluruS <>
2020-12-11 07:40:21 +0530

    Update shell-scripting/

--- roboshop-project/components/catalogue.sh
+++ roboshop-project/components/catalogue.sh
@@ -27,7 +27,7 @@ DOWNLOAD_ARTIFACT "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-
 INFO "Extract Artifacts"
 mkdir -p /home/roboshop/${COMPONENT}
 cd /home/roboshop/${COMPONENT}
-unzip /tmp/${COMPONENT}.zip &>>$LOG_FILE
+unzip -o /tmp/${COMPONENT}.zip &>>$LOG_FILE
 STAT $? "Artifacts Extract"
 
 INFO "Install NodeJS dependencies"
