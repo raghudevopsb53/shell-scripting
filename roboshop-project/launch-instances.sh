2a29ca480363f36ab262a62793af72a936e118bd
KalluruS <>
2020-12-12 08:50:33 +0530

    Update shell-scripting/

--- roboshop-project/launch-instances.sh
+++ roboshop-project/launch-instances.sh
@@ -10,6 +10,11 @@ case $1 in
   ;;
   routes)
     echo Updating routes
+    for component in frontend catalogue cart user shipping payment mysql mongo rabbitmq redis; do
+      echo "Launching $component Spot Instance"
+      IP=(aws ec2 describe-instances --filters Name=tag:Name,Values=${component} Name=instance-state-name,Values=running | jq '.Reservations[].Instances[].PrivateIpAddress')
+      echo $component $IP
+    done
   ;;
 esac
 done
