b9abf7ff904dee4c2f63599d04605541f547ef25
KalluruS <>
2020-12-12 09:10:56 +0530

    Update shell-scripting/

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -9,7 +9,7 @@ yum install nginx -y &>>$LOG_FILE
 STAT $? "Nginx Installation"
 
 INFO "Downloading Artifacts"
-DOWNLOAD_ARTIFACT "https://dev.azure.com/DevOps-Batches/f4b641c1-99db-46d1-8110-5c6c24ce2fb9/_apis/git/repositories/a781da9c-8fca-4605-8928-53c962282b74/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+DOWNLOAD_ARTIFACT "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/fdf87296-ccbe-45e5-a615-bc6ecbd78bfe/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
 
 
 INFO "Remove Old Artifacts"
