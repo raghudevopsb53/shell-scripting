656039b748d5f5d2a4fed839ecddf6a20c644256
KalluruS <>
2020-12-12 08:28:45 +0530

    Update shell-scripting/

--- roboshop-project/launch-instances.sh
+++ roboshop-project/launch-instances.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 
-for component in frontend catalogue cart user shipping payment mysql mongo rabbitmq redis; do
-  aws ec2 run-instances  --launch-template LaunchTemplateId=lt-0a3a41bad5ffd4580 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}]"
+case $1 in
+  launch)
+    for component in frontend catalogue cart user shipping payment mysql mongo rabbitmq redis; do
+    aws ec2 run-instances  --launch-template LaunchTemplateId=lt-0a3a41bad5ffd4580 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}]"
+  ;;
+  routes)
+    echo Updating routes
+  ;;
+esac
 done
 
